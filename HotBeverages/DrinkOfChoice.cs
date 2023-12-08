c68e52790ce5dd32479f449e2b1fa3f20838fce3
BuhleMcetywa <buhlemcetywa07@gmail.com>
2023-12-08 12:25:23 +0200

    First commit

---
+++ HotBeverages/DrinkOfChoice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotBeverages
+{
+    internal class DrinkOfChoice
+    {
+       private  string BeverageType;
+        private int Sugar;
+        private bool MilkOrNoMilk;
+
+        public DrinkOfChoice(string beveragetype, int sugar, bool milkornoMilk)
+        {
+            BeverageType = beveragetype;
+            Sugar = sugar;
+            MilkOrNoMilk = milkornoMilk;
+
+        }
+
+        public string GetBeverageType()
+        {
+            return BeverageType;
+        }
+
+        public int GetSugar()
+        {
+            return Sugar;
+        }
+
+        public bool GetMilkOrNoMilk() 
+        { 
+            return MilkOrNoMilk; 
+        }  
+    }
+}
---
+++ HotBeverages/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace HotBeverages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+
+            DrinkOfChoice Person1 = new DrinkOfChoice("coffee", 2, false);
+            Console.WriteLine("Iced " + Person1.GetBeverageType());
+            Console.WriteLine("How many sugars: " + Person1.GetSugar());
+            if (Person1.GetMilkOrNoMilk())
+            {
+                Console.WriteLine("This drink has milk in");
+            }
+            else
+            {
+                Console.WriteLine("This drink has no milk in");
+            }
+
+            DrinkOfChoice Person2 = new DrinkOfChoice("tea", 4, true);
+            Console.WriteLine(Person2.GetBeverageType() + " " + "is hot");
+            Console.WriteLine("How many sugars: " + Person2.GetSugar());
+            if(Person2.GetMilkOrNoMilk())
+            {
+                Console.WriteLine("This drink has  milk in");
+            }
+            else 
+            {
+                Console.WriteLine("This drink has no milk in ");
+            }
+    }
+    }
+}
